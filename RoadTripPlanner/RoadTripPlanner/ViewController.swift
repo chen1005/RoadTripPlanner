d556ad53b4229e90395b3bf429a7cf76b25014e0
chen1005 <chen1005@purdue.edu>
2015-10-07 22:13:18 -0400

    First Commit - Zhuo Chen

---
+++ RoadTripPlanner/RoadTripPlanner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RoadTripPlanner
+//
+//  Created by Rick Chen on 15/10/7.
+//  Copyright (c) 2015年 Rick Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
